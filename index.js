665a4a6b249e8b9a2288bf094d559fbc997c3aab
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2023-07-19 10:23:29 +0200

    🥳Hooray

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors()); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
